0372d9629af2cdeb7a85f8a7b06d0ae9e678f9b0
root <root@vclvm178-189.vcl.ncsu.edu>
2023-04-16 17:28:00 -0400

    added test cases for questions.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,7 @@
 class Question < ApplicationRecord
     belongs_to :questionnaire # each question belongs to a specific questionnaire
     
-    validates :seq, presence: true # user must define sequence for a question
-    validates :seq, numericality: true # sequence must be numeric
+    validates :seq, presence: true, numericality: true # sequence must be numeric
     validates :txt, length: { minimum: 0, allow_nil: false, message: "can't be nil" } # user must define text content for a question
     validates :question_type, presence: true # user must define type for a question
     validates :break_before, presence: true
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+    let(:questionnaire) { Questionnaire.new id:1, name: 'abc', private: 0, min_question_score: 0, max_question_score: 10, instructor_id: 1234 }
+    
+  describe "validations" do
+    it "is valid with valid attributes" do
+      question = Question.new(seq: 1, txt: "Sample question", question_type: "multiple_choice", break_before: true, questionnaire: questionnaire)
+      expect(question).to be_valid
+    end
+    
+    it "is not valid without a seq" do
+      question = Question.new(txt: "Sample question", question_type: "multiple_choice", break_before: true, questionnaire: questionnaire)
+      expect(question).to_not be_valid
+    end
+    
+    it "is not valid with a non-numeric seq" do
+      question = Question.new(seq: "one", txt: "Sample question", question_type: "multiple_choice", break_before: true, questionnaire: questionnaire)
+      expect(question).to_not be_valid
+    end
+    
+    it "is not valid without a txt" do
+      question = Question.new(seq: 1, question_type: "multiple_choice", break_before: true, questionnaire: questionnaire)
+      expect(question).to_not be_valid
+    end
+    
+    it "is not valid without a question_type" do
+      question = Question.new(seq: 1, txt: "Sample question", break_before: true, questionnaire: questionnaire)
+      expect(question).to_not be_valid
+    end
+    
+    it "is not valid without a break_before value" do
+      question = Question.new(seq: 1, txt: "Sample question", question_type: "multiple_choice", questionnaire: questionnaire)
+      expect(question).to_not be_valid
+    end
+    
+    it "is not valid without a questionnaire" do
+      question = Question.new(seq: 1, txt: "Sample question", question_type: "multiple_choice", break_before: true)
+      expect(question).not_to be_valid
+    end
+  end
   
+  describe "#delete" do
+    it "destroys the question object" do
+      question = Question.create(seq: 1, txt: "Sample question", question_type: "multiple_choice", break_before: true, questionnaire: questionnaire)
+      expect { question.delete }.to change { Question.count }.by(-1)
+    end
+  end
 end
